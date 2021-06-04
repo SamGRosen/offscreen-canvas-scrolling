f4272fe3dd692bb123856ddf989d819d4dac972a
Sam Rosen <rosen.samuel@gene.com>
2021-06-04 11:30:33 -0400

    Updates

--- canvas.bc8eb3cb.js
+++ canvas.bc8eb3cb.js
@@ -1419,7 +1419,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64543" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49408" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
